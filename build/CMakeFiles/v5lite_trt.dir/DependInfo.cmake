47e22be3d4eec256e14ba9121db25fcc946ea640
breakmyhabits <hnbcfzwx@163.com>
2026-02-12 21:01:29 +0800

    tensorrt

---
+++ build/CMakeFiles/v5lite_trt.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/media/F/hbf/YOLOv5-Lite-master/cpp_demo/tensorrt/main.cpp" "/media/F/hbf/YOLOv5-Lite-master/cpp_demo/tensorrt/build/CMakeFiles/v5lite_trt.dir/main.cpp.o"
+  "/media/F/hbf/YOLOv5-Lite-master/cpp_demo/tensorrt/v5lite.cpp" "/media/F/hbf/YOLOv5-Lite-master/cpp_demo/tensorrt/build/CMakeFiles/v5lite_trt.dir/v5lite.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/cuda/include"
+  ".././includes/common"
+  "/usr/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
